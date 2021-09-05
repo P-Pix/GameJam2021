ebcdf1f878893ccb6227b5a4407109825c1ebb99
P-Pix <Guillaume200202@gmail.com>
2021-09-05 19:26:21 +0200

    ready to starting

---
+++ src/Window/Constructor.cpp
@@ -0,0 +1,19 @@
+/**
+ * @file Constructor.cpp
+ * @author Guillaume LEMONNIER
+ * @date 2021-09-05
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+#include "../../include/Window.hpp"
+
+Window::Window(void)
+{
+    
+}
+Window::~Window(void)
+{
+
+}
---
+++ src/Window/Open.cpp
@@ -0,0 +1,15 @@
+/**
+ * @file Open.cpp
+ * @author Guillaume LEMONNIER
+ * @date 2021-09-05
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+#include "../../include/Window.hpp"
+
+bool Window::isOpen(void)
+{
+    return false;
+}
