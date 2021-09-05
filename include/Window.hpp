21f3b705d334b39132e5094cc8a81f9774c8f5ed
P-Pix <Guillaume200202@gmail.com>
2021-09-05 18:54:12 +0200

    First Commit

---
+++ include/Window.hpp
@@ -0,0 +1,28 @@
+/**
+ * @file Window.hpp
+ * @author Guillaume LEMONNIER
+ * @date 2021-09-05
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+#ifndef _WINDOW_HPP_
+#define _WINDOW_HPP_
+
+class Window
+{
+    private:
+
+    protected:
+
+    public:
+
+        /// Default Constructor
+        Window(void);
+
+        /// Default Destructor
+        ~Window(void);
+};
+
+#endif
---
+++ main.cpp
@@ -0,0 +1,19 @@
+/**
+ * @file main.cpp
+ * @author Guillaume LEMONNIER
+ * @date 2021-09-05
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+// Theme Join Together
+
+#include "SDL2-2.0.16/include/SDL.h"
+
+int main(void);
+
+int main(void)
+{
+    
+}
